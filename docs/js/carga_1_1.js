0e650ab6c5afa4e80473902ab5e8e62f18334849
Felipe Gomez <feli.gomezr@duocuc.cl>
2021-05-20 19:03:48 -0400

    se agrego info

---
+++ docs/js/carga_1_1.js
@@ -0,0 +1,47 @@
+$('#submit-form').click(function(event) {
+
+    event.preventDefault();
+    var movie = $("#movie").val();
+
+            // http://www.omdbapi.com/?t=godfather&apikey=2f72185
+   $.ajax({
+      url: 'http://www.omdbapi.com/?t='+movie+'&apikey=2f72185',
+      data: {
+         format: 'json'
+      },
+      error: function() {
+         $('#info').html('<p>Un error ha ocurrido!!!!</p>');
+      },
+      dataType: 'json',
+      success: function(data) {
+         console.log(data);
+         var $caratula = $("<p><img class='caratula' src='"+data.Poster+"'>");
+         var $nombre_pelicula = $('<h1>').text(data.Title);
+         var $lanzamiento = $('<p>').text(data.Released);
+         var $duracion = $('<p>').text(data.Runtime);
+         var $director = $('<p>').text(data.Director);
+         var $escritor = $('<p>').text(data.Writer);
+         var $actores = $('<p>').text(data.Actors);
+         var $trama = $('<p>').text(data.Plot);
+         var $premios = $('<p>').text(data.Awards);
+         
+         
+
+        // para limpiar el contedor antes de desplegar
+        $("#info").empty();
+         $('#info')
+            .append($caratula)
+            .append($nombre_pelicula)
+            .append($lanzamiento)
+            .append($duracion)
+            .append($director)
+            .append($escritor)
+            .append($actores)
+            .append($trama)
+            .append($premios);
+            
+      },
+      type: 'GET'
+   });
+});
+
